e087f4330585d57d60af5be708a86621402ad1ae
fernandodames@gmail.com <Fernando Dames>
2021-06-30 20:35:05 -0300

    TP Individual #2

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req,res) =>{
+   res.sendFile(path.join(__dirname, './views/home.html'));  // Permite enviar un archivo HTML
+});
+
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'register'));  // Permite enviar un archivo HTML
+});
+
+
+app.use(express.static(path.join(__dirname, './public')));  
+
+app.use(express.static(path.join(__dirname, './views')));
+
+
+/* app.listen(3000, () => {
+    console.log("Servidor corriendo");
+}); */
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+});
