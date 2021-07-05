3108a9cd912a48bc1780bfb6284eb1000a6c29ba
Fernando Dames <Fernando Dames>
2021-07-05 19:44:02 -0300

    update

--- app.js
+++ app.js
@@ -4,22 +4,17 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req,res) =>{
-   res.sendFile(path.join(__dirname, './views/home.html'));  // Permite enviar un archivo HTML
+   res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
 app.get('/register', (req,res) =>{
-    res.sendFile(path.join(__dirname, 'register'));  // Permite enviar un archivo HTML
+    res.sendFile(path.join(__dirname, 'register')); 
 });
 
 
 app.use(express.static(path.join(__dirname, './public')));  
 
 app.use(express.static(path.join(__dirname, './views')));
-
-
-/* app.listen(3000, () => {
-    console.log("Servidor corriendo");
-}); */
 
 app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo");
